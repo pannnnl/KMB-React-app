1bd7f390f423ff561753e592a233239265e7f612
pannnnl <48431027+pannnnl@users.noreply.github.com>
2025-05-23 01:05:09 +0800

    rename - BusClock

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -6,6 +6,7 @@ function Logo() {
         src="https://upload.wikimedia.org/wikipedia/en/thumb/b/b4/KowloonMotorBus.svg/2560px-KowloonMotorBus.svg.png"
         alt="KMB Logo"
       />
+      <h1 className="text-rose-600 text-2xl font-bold">BusClock</h1>
     </div>
   );
 }
